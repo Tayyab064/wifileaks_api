d415d68937bd37017a16a47550189439f5e46fd4
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-07-11 18:09:29 +0500

    error reporting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :restrict_access, except: [:create , :wifi_user]
+	before_filter :restrict_access, except: [:create]
 
 	def index
 		render json: @current_user

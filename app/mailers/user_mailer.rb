fa99b7b15311d38e5ba1fa5e0f78aa14706c8cbe
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-08-12 16:31:05 +0500

    email templete

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 	def registration_confirmation(user)
 		@user = user
     	@url  = 'https://wifi-api.herokuapp.com'
-    	mail(to: @user.email, subject: 'WifiExplore', body: 'Welcome '+ @user.first_name + '. Please click on link to confirm email: https://wifi-api.herokuapp.com/user/verify/email/'+ @user.verification.email_token )
+    	mail(to: @user.email, subject: 'WifiExplore' )
 	end
 
 	def reset_password(user)
@@ -11,3 +11,7 @@ class UserMailer < ApplicationMailer
     	mail(to: @user.email, subject: 'WifiExplore', body: 'Welcome again '+ @user.first_name + '. Please click on link to reset password: https://wifi-api.herokuapp.com/user/reset_password/'+ @user.verification.forgot_password_token )
 	end
 end
+
+#https://wifi-api.herokuapp.com/user/verify/email/'+ @user.verification.email_token
+#{:email => @user.verification.email_token, :url => "https://wifi-api.herokuapp.com/email_confirmation.html"}
+#, body: '<%= render template: "email_confirmation.html" %>'
